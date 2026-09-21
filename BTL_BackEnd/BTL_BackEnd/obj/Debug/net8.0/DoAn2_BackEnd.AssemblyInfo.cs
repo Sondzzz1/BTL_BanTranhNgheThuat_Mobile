09bb5d8b67099afe777ea81a3c8a4e92d4d29d40
Sondzzz1 <phanduyson123456789@gmail.com>
2026-09-21 08:10:04 +0700

    doi lai ten du an

--- BTL_BackEnd/BTL_BackEnd/obj/Debug/net8.0/DoAn2_BackEnd.AssemblyInfo.cs
+++ BTL_BackEnd/BTL_BackEnd/obj/Debug/net8.0/DoAn2_BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAn2_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e53c5f06e394f79e135b44e4bd2c2b1bc387c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41eb3e937d2d0f0cd126346d43cbb34ec0f05733")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAn2_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAn2_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
